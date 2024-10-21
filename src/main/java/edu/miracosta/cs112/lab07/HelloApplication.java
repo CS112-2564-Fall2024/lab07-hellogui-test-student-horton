7a939b12bb2a45467d56023f77abd9397b761c17
Kristopher Horton <khorton@miracosta.edu>
2024-10-21 15:52:43 -0700

    Test

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -27,7 +27,6 @@ public class HelloApplication extends Application  { //inheriting core functiona
         Scene scene = new Scene(layout, 400,400);
         primaryStage.setScene(scene);
         primaryStage.show();
-        // temp
     }
 
     // TODO: follow steps 2-9 in README.md to create a start method
