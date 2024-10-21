144d61a6be7f89655f5e9140f9703c5bf70d3112
Kristopher Horton <khorton@miracosta.edu>
2024-10-21 15:59:24 -0700

    Test

--- src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
+++ src/main/java/edu/miracosta/cs112/lab07/HelloApplication.java
@@ -24,6 +24,8 @@ public class HelloApplication extends Application  { //inheriting core functiona
 
         StackPane layout = new StackPane();
 
+
+
         Scene scene = new Scene(layout, 400,400);
         primaryStage.setScene(scene);
         primaryStage.show();
